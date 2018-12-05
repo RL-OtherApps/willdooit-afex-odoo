70e99a75271fc7fae91ec2e1807ba897a8824d95
Richard deMeester <richardd@willdooit.com>
2018-12-05 14:31:30 +1100

    T0013 / #65 variance in settlment amount (#16)
    Ensure currency precision used when rounding settlment amount

--- afex_integration/__manifest__.py
+++ afex_integration/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "AFEX Global Vendor Payments for Businesses",
-    "version": "10.0.1.3",
+    "version": "10.0.1.4",
     "summary": "Make payments to vendors in a foreign currency using AFEX, "
                "by WilldooIT",
     "description": """
